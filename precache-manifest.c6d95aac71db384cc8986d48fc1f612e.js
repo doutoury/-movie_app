e430f3cc4bd7ba374fe76b48f6bc5c0fbc621f97
doutoury <doutoury@naver.com>
2020-10-01 01:11:32 +0900

    Updates

---
+++ precache-manifest.c6d95aac71db384cc8986d48fc1f612e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb62e8789bb184cf1df2351062cb5269",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "91f70d7f6acf668bb036",
+    "url": "/movie_app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f70d7f6acf668bb036",
+    "url": "/movie_app/static/js/main.34f28585.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
